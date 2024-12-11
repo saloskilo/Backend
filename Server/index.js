be3f0506b4a06ea48247c4da1826b4f12ea37e1c
Salman Ali <salmanafridi945@gmail.com>
2024-12-12 01:25:41 +0500

    install express from the npm and made server which make easy everything to make a server.

--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,54 @@
-const http = require("http");
-const fs = require('fs');
-const url=require('url')
+// const http = require("http");
+// const fs = require('fs');
+// const url=require('url')
 
-const myServer = http.createServer((req, res) => {
-    fs.appendFile('./logs.txt', `${req.url}\n`, (err) => {
-        console.log(err);
-    })
-    const myURL=url.parse(req.url,true);
-    console.log(myURL);
-    switch (myURL.pathname) {
-        case '/':
-            res.end("This is Home page")
-            break;
-        case '/about':
-            const userName=myURL.query.myname;
-            res.end(`This is ${userName}`);
-            break;
-        default: res.end("404 not found");
-            break;
-    }
-    // res.end("Server is started again")
 
+// function handler(req, res) {
+//     fs.appendFile('./logs.txt', `${req.url}\n`, (err) => {
+//         console.log(err);
+//     })
+//     const myURL=url.parse(req.url,true);
+//     console.log(myURL);
+//     switch (myURL.pathname) {
+//         case '/':
+//             res.end("This is Home page")
+//             break;
+//         case '/about':
+//             const userName=myURL.query.myname;
+//             res.end(`This is ${userName}`);
+//             break;
+//         default: res.end("404 not found");
+//             break;
+//     }
+//     // res.end("Server is started again")
+
+// }
+// const myServer = http.createServer(handler);
+
+// myServer.listen(8000, () => {
+//     console.log("Server is started");
+
+// })
+
+
+
+
+
+
+// ------------------------------Express Started ------------------------------//
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    return res.end(`'This is Home Page '${req.query.myname}`)
 });
 
-myServer.listen(8000, () => {
-    console.log("Server is started");
 
+app.get('/about', (req, res) => {
+    return res.end('This is about  Page')
+});
+
+app.listen(8000, () => {
+    console.log("Server Started");
 })
